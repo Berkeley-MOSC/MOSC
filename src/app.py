584617703e349fd3b04e5b8520a00e2d8b6c9c06
Nicholas Fong <nickfong@users.noreply.github.com>
2016-04-11 14:54:22 -0700

    Update endpoints to use arguments

--- src/app.py
+++ src/app.py
@@ -1,24 +1,58 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/api/v1/http/<path:url>')
-def url_endpoint(url):
-    pass
+@app.route('/api/v1/http/')
+def url_endpoint():
+    url = request.args.get('url')
+    if pass_connection('url'):
+        # Generate request to the URL, serve back to user
+        pass
+    else:
+        generate_503()
 
-@app.route('/api/v1/sms/<path:number>/<path:message>')
-def sms_endpoint(number, message):
-    pass
+@app.route('/api/v1/sms/')
+def sms_endpoint():
+    number = request.args.get('number')
+    message = request.args.get('message')
+    if pass_connection("sms"):
+        pass
+    else:
+        reject_sms()
 
-@app.route('/api/v1/call/<path:number>/<path:message>')
-def call_endpoint(number, message):
-    pass
+@app.route('/api/v1/call/')
+def call_endpoint():
+    number = request.args.get('number')
+    message = request.args.get('message')
+    if pass_connection("call"):
+        pass
+    else:
+        reject_call()
 
-@app.route('/api/v1/emergency_call/<path:message>')
-def e_call_endpoint(number, message):
-    pass
+@app.route('/api/v1/emergency_call/')
+def e_call_endpoint():
+    number = request.args.get('number')
+    if pass_connection("ecall"):
+        pass
+    else:
+        reject_ecall()
 
-@app.route('/api/v1/stats')
+@app.route('/api/v1/stats/')
 def return_stats():
+    pass
+
+def pass_connection(connection_type):
+    pass
+
+def generate_503():
+    pass
+
+def reject_sms():
+    pass
+
+def reject_call():
+    pass
+
+def reject_ecall():
     pass
 
 if __name == '__main__':
