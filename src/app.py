d4a13556d8db8e1db11cbfd7934e0157d6361ad3
Nicholas Fong <nickfong@users.noreply.github.com>
2016-04-14 15:21:12 -0700

    Implement route handlers

--- src/app.py
+++ src/app.py
@@ -1,49 +1,63 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
+################### Routes #####################
 @app.route('/api/v1/http/')
 def url_endpoint():
     url = request.args.get('url')
     if pass_connection('url'):
         # Generate request to the URL, serve back to user
-        pass
+        req = request.get(url, stream = True)
+        return Response(stream_with_context(req.iter_content()), 
+                content_type = req.headers['content-type'])
     else:
-        generate_503()
+        return generate_503()
 
 @app.route('/api/v1/sms/')
 def sms_endpoint():
     number = request.args.get('number')
     message = request.args.get('message')
     if pass_connection("sms"):
-        pass
+        return send_sms(number, message)
     else:
-        reject_sms()
+        return reject_sms()
 
 @app.route('/api/v1/call/')
 def call_endpoint():
     number = request.args.get('number')
     message = request.args.get('message')
     if pass_connection("call"):
-        pass
+        return send_call(number, message)
     else:
-        reject_call()
+        return reject_call()
 
 @app.route('/api/v1/emergency_call/')
 def e_call_endpoint():
     number = request.args.get('number')
     if pass_connection("ecall"):
-        pass
+        return send_ecall(number)
     else:
-        reject_ecall()
+        return reject_ecall()
 
 @app.route('/api/v1/stats/')
 def return_stats():
     pass
 
+################### Responses #####################
 def pass_connection(connection_type):
     pass
 
 def generate_503():
+    pass
+
+def send_sms(number, message):
+    pass
+
+def send_call(number, message):
+    pass
+
+def send_ecall(number):
     pass
 
 def reject_sms():
